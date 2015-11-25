c25f6be2f823e1b98b4006de02e24da3241aacdd
jaredmckaychristensen <jared.christensen@byu.edu>
2015-11-25 14:49:22 -0800

    managers connects to database and displays contents of database

--- Database-2015-11-23/Database/hello.py
+++ Database-2015-11-23/Database/hello.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template
+from flaskext.mysql import MySQL
+from flask import Markup
+
 app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '350-databases'
+app.config['MYSQL_DATABASE_DB'] = 'marketing'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route('/')
 def hello_world():
@@ -7,11 +16,19 @@ def hello_world():
 
 @app.route('/signin', methods=['GET'])
 def hello_earth():
+		#message = Markup("<h1>TEST</h1>")
+	#flash(message)
 	return render_template('signin.html')
+
 
 @app.route('/managers', methods=['GET'])
 def hello_joseph():
-	return render_template('Managers.html')
+	conn = mysql.connect()
+	cursor =conn.cursor()
+	cursor.execute("SELECT * from Marketing")
+	data = cursor.fetchall()
+
+	return render_template('Managers.html',data=data)
 
 
 if __name__ == '__main__':
